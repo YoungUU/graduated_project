e682498d3bf372efe0ce9448caa162c9a70487ac
wushijia <whalewsj@gmail.com>
2019-04-21 14:53:02 +0800

    wushijia

--- my-graduation-project-web-admin/src/main/java/com/cqupt/ysc/graduation/project/web/admin/dao/ProjectDao.java
+++ my-graduation-project-web-admin/src/main/java/com/cqupt/ysc/graduation/project/web/admin/dao/ProjectDao.java
@@ -1,7 +1,16 @@
 package com.cqupt.ysc.graduation.project.web.admin.dao;
 
+import com.cqupt.ysc.graduation.project.domain.Project;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectDao {
+
+    List<Project> getProject(String email);
+    Project getProjectById(long id);
+    void deleteProjectById(long id);
+    void saveProject(Project project);
+
 }
--- my-graduation-project-web-admin/src/main/java/com/cqupt/ysc/graduation/project/web/admin/dao/RewardDao.java
+++ my-graduation-project-web-admin/src/main/java/com/cqupt/ysc/graduation/project/web/admin/dao/RewardDao.java
@@ -1,7 +1,14 @@
 package com.cqupt.ysc.graduation.project.web.admin.dao;
 
+import com.cqupt.ysc.graduation.project.domain.Reward;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RewardDao {
+    List<Reward> getReward(String email);
+    Reward getRewardById(long id);
+    void deleteRewardById(long id);
+    void saveReward(Reward reward);
 }
